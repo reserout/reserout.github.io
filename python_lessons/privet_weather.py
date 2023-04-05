ebcc55d4bfe86c96626c5539d31c83afc49063db
reserout <reserout@gmail.com>
2023-04-04 21:30:44 -0400

    b

---
+++ python_lessons/privet_weather.py
@@ -0,0 +1,10 @@
+name = "Serhii"
+weather = "Облачно"
+sum = 111.5
+
+#print("Привет, " + name + "!")
+#print("Сейчас на улице " + weather + ", а у тебя в кармане " + str(sum) + "$")
+
+# f-строка
+print(f"Привет, {name}!")
+print(f"Сейчас на улице {weather}, а у тебя в кармане {sum}$")
