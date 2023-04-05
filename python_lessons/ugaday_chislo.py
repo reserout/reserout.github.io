c47597fd303192b7fd4f2b4a8966a11f5e4a4c55
reserout <reserout@gmail.com>
2023-04-04 23:02:26 -0400

    i

--- python_lessons/ugaday_chislo.py
+++ python_lessons/ugaday_chislo.py
@@ -2,3 +2,10 @@ import random
 
 random_number = random.randint(1, 5)
 user_number = input("Угадай число (от 1 до 5): ")
+
+if user_number == random_number:
+    print("Вы угадали!")
+    
+else:
+    print("Вы НЕ угадали")
+    print(f"Было загадано число {random_number}")
