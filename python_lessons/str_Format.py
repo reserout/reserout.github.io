a3004e7b5e33feebf63c53eb57d898f46f387bbf
reserout <reserout@gmail.com>
2023-04-16 18:29:40 -0400

    м

---
+++ python_lessons/str_Format.py
@@ -0,0 +1,7 @@
+age = 33
+name = 'reser'
+
+print('Возраст {0} -- {1} лет.' .format(name, age))
+print('Почему {0} балуется с Python?'.format(name))
+
+print('{name} написал {book}'.format(name='Swaroop', book='A Byte of Python'))
